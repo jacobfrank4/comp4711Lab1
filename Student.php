e7785570378e173b6561420cb8499f324b8e06ce
frank <frank@142.232.133.2>
2017-01-12 21:44:48 -0800

    Commented

--- Student.php
+++ Student.php
@@ -1,7 +1,9 @@
 <?php
 
 /**
- * Description of Student
+ * Class defines a student
+ * Providing the Student with a:
+ * Name, email, and grades
  * @author Jacob
  */
 class Student {
@@ -13,14 +15,17 @@ class Student {
         $this->grades = array();
     }
     
+    //Add an email to the Student's email array
     function add_email($which, $address) {
         $this->emails[$which] = $address;
     }
     
+    //Adds a grade to the grades array
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    //Calculates grade average
     function average() {
         $total = 0;
         foreach ($this->grades as $value)
@@ -28,6 +33,10 @@ class Student {
         return $total / count($this->grades);
     }
     
+    //Prints a description of the student
+    //Name (average)
+    //home contact 
+    //work contact
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' ('.$this->average().")\n";
